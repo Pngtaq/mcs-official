4e682e4bed60bb21975961d12301900e073b098f
Pngtaq <110612377+Pngtaq@users.noreply.github.com>
2025-10-25 22:36:44 +0800

    feat: middle page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import Navigation from "../../components/Navigation";
+import MiddlePage from "../../components/home/MiddlePage";
 import TopPage from "../../components/home/topPage";
 function Home() {
   return (
     <div>
-      <div className="bg-white rounded-b-[60px] sm:rounded-b-[140px] flex flex-col pb-20 lg:pb-0 2xl:h-screen ">
+      <div className="bg-white rounded-b-[60px] sm:rounded-b-[140px] flex flex-col pb-20 lg:pb-0 2xl:min-h-screen justify-between">
         <Navigation />
         <TopPage />
       </div>
 
-      <div className="h-screen">domo</div>
+      <div>
+        <MiddlePage />
+      </div>
     </div>
   );
 }
